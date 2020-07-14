b7dbfb4ac13a7ca17487649c21975ab3a22eb579
malarc01 <email@example.com>
2020-07-14 14:22:22 -0400

    add bodyParser

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const express = require('express')
 const app = express();
 const path = require('path')
 const port = 3333
+const bodyParser = require('body-parser');
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded());
+
+
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
+
+
+app.post('/url', function (req, res) {
+  const url = req.body.url;
+
+  res.send(url);
+});
 
 
 app.listen(port, () => console.log(`url-short listening on port ${port}!`));
